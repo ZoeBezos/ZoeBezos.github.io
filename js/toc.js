e94effc386337f7aef0900748b21149891102383
zoebezos <zoe20180314@gmail.com>
2023-06-10 12:02:27 +0800

    Site updated: 2023-06-10 12:02:26

--- js/toc.js
+++ js/toc.js
@@ -1,16 +1,47 @@
-const tocWrap = document.getElementById('post-toc');
+const tocEl = document.querySelector('#toc')
+const contentEl = document.querySelector('#postBody')
 
-function toggleToc(button, expend, collapse) {
-  let expaned = tocWrap.getAttribute('data-expand') == "true";
-  tocWrap.setAttribute('data-expand', !expaned);
+window.addEventListener('scroll', adjustToc)
+window.addEventListener('resize', adjustToc)
+window.addEventListener('resize', updateData)
+window.addEventListener('load', updateData)
 
-  if (expaned) {
-    button.innerText = collapse;
+
+let tocOffsetTop = getOffsetTop(tocEl)
+let articleHeight = contentEl.offsetHeight;
+let tocHeight = tocEl.offsetHeight;
+
+let breakPoint1 = tocOffsetTop
+let breakPoint2 = articleHeight + tocOffsetTop - tocHeight
+
+function updateData() {
+  tocOffsetTop = getOffsetTop(tocEl)
+  articleHeight = contentEl.offsetHeight;
+  tocHeight = tocEl.offsetHeight;
+
+  breakPoint1 = tocOffsetTop
+  breakPoint2 = articleHeight + tocOffsetTop - tocHeight
+}
+
+function getOffsetTop(el) {
+  let offsetTop = 0
+  while (el != null) {
+    offsetTop += el.offsetTop
+    el = el.offsetParent
+  }
+  return offsetTop
+}
+
+
+function adjustToc() {
+  if (window.scrollY < breakPoint1) {
+    tocEl.className = 'post-toc'
+    tocEl.style.top = '0'
+  } else if (window.scrollY < breakPoint2) {
+    tocEl.className = 'post-toc--attached'
+    tocEl.style.top = '0'
   } else {
-    window.scroll({
-      top: tocWrap.offsetTop - 65,
-      behavior: "smooth"
-    });
-    button.innerText = expend;
+    tocEl.className = 'post-toc--bottom'
+    tocEl.style.top = articleHeight - tocHeight + 'px'
   }
 }
